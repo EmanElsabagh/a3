2479620ecdc470cc8b113fb43c9c1bbf6f730f66
Eman <emanelsabagh99@gmail.com>
2023-10-01 14:00:07 +0200

    finalll

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class Myhomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: A2(),
     );
   }
@@ -102,22 +103,100 @@ class _A2State extends State<A2> {
                   ],
                 ),
               ),
-           GridView.count(
-             crossAxisSpacing: 10,
-             mainAxisSpacing: 10,
-             crossAxisCount: 2,
-           children: [
-             Container(
-               padding: const EdgeInsets.all(8),
-               color: Colors.teal[200],
-               child: const Text('Heed not the rabble'),
-             ),
-           // gridy("\$0.00", "Presenting Withdraw"),
-            //gridy("\$250", "Withdrawwww"),
-            //gridy("\$156", "excellent Casty from Customer"),
-            //gridy('\$300', "Total Enraning"),
+           Container(
+             height: 50,
+             child: GridView.count(
+               shrinkWrap: true,
+               crossAxisSpacing: 10,
+               mainAxisSpacing: 10,
+               crossAxisCount: 2,
+             children: [
+              gridy("\$0.00", "Presenting Withdraw"),
+              gridy("\$250", "Withdrawwww"),
+              gridy("\$156", "excellent Casty from Customer"),
+              gridy('\$300', "Total Enraning"),
           ],
         ),
+           ),
+              SizedBox(height: 15.0,),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Withdraw History", style: TextStyle(fontWeight: FontWeight.bold),),
+                    TextButton(
+                      onPressed: (){
+                        showModalBottomSheet(context: context, builder: (BuildContext context){
+                          return SizedBox(
+                            height: 800,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Text("Withdraw", style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
+                                  Image.asset("assets/images/i2.jpeg"),
+                                  const Text("Bank America",style: TextStyle(fontSize: 15.0),),
+                                  const Text("branch: Dubai",style: TextStyle(color: Colors.grey),),
+                                  const Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text("Account Number: ",style: TextStyle(color: Colors.grey),),
+                                      Text("01228899015",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                  SizedBox(height: 15.0,),
+                                  const Text("Enter Amount", style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+                                  SizedBox(height: 15.0,),
+                                  SizedBox(height: 15.0,),
+                                  Container(
+                                    width: double.infinity,
+                                    margin: EdgeInsets.all(20.0),
+                                    child: ElevatedButton(
+                                      child: const Text('Withdraw',style: TextStyle(color: Colors.white),),
+                                      onPressed: () => Navigator.pop(context),
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Colors.deepOrange,
+                                    ),
+                                  ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                      },
+                        child: const Text("View All",style: TextStyle(color: Colors.deepOrange),),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15.0,),
+              Container(
+               child: ListView(
+                 shrinkWrap: true,
+                 children: [
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Success",Colors.green),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Success",Colors.green),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Failed",Colors.red),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "pending",Colors.red),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Success", Colors.green),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Failed",Colors.red),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Failed",Colors.red),
+                   SizedBox(height: 8.0,),
+                   listt("\$145.00", "28-5-2021", "transfer to bank name", "Success", Colors.green),
+                 ],
+               ),
+             ),
+
+
             ],
           ),
 
@@ -126,15 +205,54 @@ class _A2State extends State<A2> {
     );
   }
 }
+
+
+
+
 Widget gridy(txt,subtxt) {
   return Container(
-    color: Colors.grey,
+    height: 10,
+    width: 10,
+    color: Colors.white70,
     child: Column(
       children: [
-        Text(txt, style: TextStyle(color: Colors.deepOrange),),
-        Text(subtxt,style: TextStyle(color: Colors.deepOrange),),
+        Text(txt, style: TextStyle(color: Colors.deepOrange,fontSize: 10.0),),
+        Text(subtxt,style: TextStyle(color: Colors.grey,fontSize: 10.0),),
       ],
     ),
   );
-
 }
+Widget listt(txt1,subtext1,txt2,subtxt2,color){
+  return Container(
+    padding: EdgeInsets.all(10.0),
+    margin: EdgeInsets.all(2.0),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(30.0),
+      border: Border.all(width: 2.0, color: Colors.grey),
+    ),
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(txt1, style: TextStyle(color: Colors.black),),
+            Text(subtext1, style: TextStyle(color: Colors.grey),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(txt2,style: TextStyle(color: Colors.grey),),
+                Text(subtxt2,style: TextStyle(color: color),),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+
+
+
+
